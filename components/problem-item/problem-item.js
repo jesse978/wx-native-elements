6a6786ec536115d729e8cfeca0d911b095f26004
jesse <386018651@qq.com>
2022-07-18 14:20:10 +0800

    删除部分无用组件

--- components/problem-item/problem-item.js
+++ components/problem-item/problem-item.js
@@ -7,32 +7,32 @@
  */
 // components/problem-item/problem-item.js
 Component({
-  externalClasses: ['qt-class'],
-  /**
-   * 组件的属性列表
-   */
-  properties: {
-    item: {
-      type: Object,
-      value: {},
+    externalClasses: ['qt-class'],
+    /**
+     * 组件的属性列表
+     */
+    properties: {
+        item: {
+            type: Object,
+            value: {},
+        },
+        isRoom: {
+            type: Boolean,
+            value: true
+        },
     },
-    isRoom:{
-      type:Boolean,
-      value:true
-    },
-  },
 
-  /**
-   * 组件的初始数据
-   */
-  data: {},
+    /**
+     * 组件的初始数据
+     */
+    data: {},
 
-  /**
-   * 组件的方法列表
-   */
-  methods: {
-    problemDetail(e) {
-      this.triggerEvent('click', { item: this.data.item })
+    /**
+     * 组件的方法列表
+     */
+    methods: {
+        problemDetail(e) {
+            this.triggerEvent('click', {item: this.data.item})
+        },
     },
-  },
 })
